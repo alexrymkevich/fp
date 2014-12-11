f79bdfc1d6c1f0290797c9923052be90afe33f6d
Alex Rymkevich <alex.rymkevich@gmail.com>
2014-12-11 04:41:28 +0300

    first lr Clusters

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rymkevich_fp_1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/rymkevich_fp_1/core.clj
@@ -0,0 +1,111 @@
+(ns rymkevich-fp-1.core)
+
+(def Ra 3)
+(def Rb (* 1.5 Ra))
+
+(defn sqr [x]
+  (* x x))
+
+(def alfa (/ 4 (sqr Ra)))
+(def betta (/ 4 (sqr Rb)))
+
+
+(defn calc-potential [distance]
+    (Math/exp (- (* alfa distance))))
+
+(defn calc-potential-with-betta [distance]
+  (Math/exp (- (* betta distance))))
+
+
+
+; work with file
+(defn read-coord-from-line [str]
+  (vector
+   (hash-map :coord
+             (map read-string
+                  (butlast
+                   (map clojure.string/trim
+                        (clojure.string/split str #",")))))
+
+      ))
+
+
+(defn read-coordinates-from-file [fileName]
+  (mapcat read-coord-from-line
+          (line-seq
+           (clojure.java.io/reader fileName)
+       )))
+
+
+;function distance
+(defn euclidian-distance
+  [c1 c2]
+  ( reduce + (map sqr  (map - c1 c2) )))
+
+(defn hamming-distance
+  [c1 c2]
+  (count (filter true? (map not= c1 c2))))
+
+
+
+
+(defn calc-potentials [points method]
+  (map #(calc-point-potential %1 points method) points))
+
+
+(defn revise-potential [point kernel method]
+  (assoc point :dist
+       (- (:dist point))
+       (* (:dist kernel))
+       (calc-potential-with-betta
+        (method (:coord point) (:coord kernel))))
+       )
+
+
+(defn revise-point-potentials [points kernel method]
+  (reverse
+     (sort-by :dist
+              (map #(revise-potential %1 kernel method) points))
+       ))
+
+
+(defn calc-min-dist [point points method]
+  (apply min (map #(method (:coord point) (:coord %1)) points)))
+
+
+;algorithm
+(defn process [points method]
+  (let [initPotentials (reverse (sort-by :dist (calc-potentials points method)))
+        firstKernel (first initPotentials)]
+      (loop [kernels [firstKernel]
+             elements (rest initPotentials)]
+        (let [revisedPoints (revise-point-potentials elements (first kernels) method)
+              nextKernel (first revisedPoints)]
+            (cond
+              (< (:dist nextKernel) (* 0.15 (:dist firstKernel)))
+              (reverse (sort-by :dist kernels))
+              (> (:dist nextKernel) (* 0.5 (:dist firstKernel))) (recur (cons nextKernel kernels) (rest revisedPoints))
+              (>= (+ (/ (calc-min-dist nextKernel kernels method) Ra) (/ (:dist nextKernel) (:dist firstKernel))) 1) (recur (cons nextKernel kernels) (rest revisedPoints))
+              :else (recur kernels (cons (assoc nextKernel :dist 0) (rest revisedPoints))))))))
+
+
+; MAIN FUNCTION
+(defn -main [fileName distanceMethod]
+  (let [points (read-coordinates-from-file fileName)
+        distance (if (= distanceMethod "h") hamming-distance euclidian-distance)]
+ (process points distance)))
+
+
+; FOR DEBUG
+
+;(-main "c:/Users/Alex/Downloads/clojure-1.6.0/rymkevich_fp_1/src/rymkevich_fp_1/butterfly.txt" "h")
+
+; (read-coordinates-from-file "c:/Users/Alex/Downloads/clojure-1.6.0/rymkevich_fp_1/src/rymkevich_fp_1/butterfly.txt")
+
+
+;(defn clusterEstimation
+; ([]  (println "Incorrect start params"))
+; ([points ]  (println "Incorrect start params"))
+;  ([points  distanceMethod]
+;    println (process points distanceMethod)))
+
